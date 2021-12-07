ae2020c4247c1bfab92aa6e7dd3f7d939d4e4b92
Abi <91884406+Abinaya672@users.noreply.github.com>
2021-12-07 21:10:05 +0530

    Update and rename README.md to main.dart

---
+++ main.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Stateful widget",
+    home: Favoritecity(),
+  ));
+}
+
+
+class Favoritecity extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _FavoritecityState();
+  }
+}
+
+class _FavoritecityState extends State<Favoritecity> {
+  String namecity = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateful app example"),
+      ),
+      body: Container(
+        margin: EdgeInsets.all(20.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: (String userInput) {
+                setState(() {
+                  namecity = userInput;
+                });
+              },
+            ),
+            Padding(
+              padding: EdgeInsets.all(30.0),
+              child: Text(
+                "Your best city is  $namecity",
+                style: TextStyle(fontSize: 20.0),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
